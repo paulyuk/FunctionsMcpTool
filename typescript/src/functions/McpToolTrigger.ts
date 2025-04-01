bd6e17a323b99c6cabb64f7cd31a58c485b9b7b8
paulyuk <paulyuk@microsoft.com>
2025-03-31 18:00:49 -0700

    Created Hello trigger and stubbed out SnippetTool conversion

--- typescript/src/functions/McpToolTrigger.ts
+++ typescript/src/functions/McpToolTrigger.ts
@@ -1,15 +1,28 @@
 import { app, InvocationContext, output } from "@azure/functions";
 
-export async function mcpToolTrigger(context: InvocationContext): Promise<string> {
+export async function mcpToolHello(context: InvocationContext): Promise<string> {
     return "Hello I am MCP Tool!";
 }
 
-app.mcpTool('mcpToolTriggerTest', {
-    toolName: 'McpToolTriggerTest',
-    description: 'MCP Tool With Azure Functions Description',
-    // return: output.storageBlob({
-    //     path: 'output/mcpOutput.txt',
-    //     connection: 'blob_connection_for_storage',
-    // }),
-    handler: mcpToolTrigger
+app.mcpTool('hello', {
+    toolName: 'hello',
+    description: 'Simple hello world MCP Tool',
+    handler: mcpToolHello
 });
+
+
+// app.mcpTool('save-snippet', {
+//     toolName: 'save-snippet',
+//     description: 'MCP Tool that saves code snippet from user to blob storage',
+//     return: output.storageBlob({
+//         path: 'snippets/mcpOutput.txt',
+//         connection: 'AzureWebJobsStorage',
+//     }),
+//     handler: mcpToolTrigger
+// });
+
+// app.mcpTool('save-snippet', {
+//     toolName: 'save-snippet',
+//     description: 'MCP Tool that saves code snippet from user to blob storage',
+//     handler: mcpToolTrigger
+// });
