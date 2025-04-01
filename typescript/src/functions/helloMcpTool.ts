c584d023c65ef331ec711a08ffe5a97ed1e8610b
paulyuk <paulyuk@microsoft.com>
2025-03-31 18:31:13 -0700

    removing dead code

--- typescript/src/functions/helloMcpTool.ts
+++ typescript/src/functions/helloMcpTool.ts
@@ -9,20 +9,3 @@ app.mcpTool('hello', {
     description: 'Simple hello world MCP Tool that responses with a hello message.',
     handler: mcpToolHello
 });
-
-// TODO: Swapnil could you please implement these to match logic in dotnet/SnippetTool.cs?
-// app.mcpTool('save_snippet', {
-//     toolName: 'save_snippet',
-//     description: 'MCP Tool that saves code snippet from user to blob storage',
-//     return: output.storageBlob({
-//         path: 'snippets/mcpOutput.txt',
-//         connection: 'AzureWebJobsStorage',
-//     }),
-//     handler: mcpToolTrigger
-// });
-
-// app.mcpTool('get_snippet', {
-//     toolName: 'get_snippet',
-//     description: 'MCP Tool that retreives code snippet from blob storage by name',
-//     handler: mcpToolTrigger
-// });
